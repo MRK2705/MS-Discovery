17da7a3b7f3b91d7456b51f54677511e872e0ba0
Mirko Alarcon <mirko.alarcon@ucb.edu.bo.com>
2023-04-13 03:26:25 -0400

    terminado servidor de configuracion

--- ConfServ/ms-config-server/src/main/kotlin/com/example/msconfigserver/MsConfigServerApplication.kt
+++ ConfServ/ms-config-server/src/main/kotlin/com/example/msconfigserver/MsConfigServerApplication.kt
@@ -2,8 +2,12 @@ package com.example.msconfigserver
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.config.server.EnableConfigServer
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 
 @SpringBootApplication
+@EnableConfigServer
+@EnableEurekaClient
 class MsConfigServerApplication
 
 fun main(args: Array<String>) {
